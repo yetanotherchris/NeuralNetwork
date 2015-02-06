229157ed83e7a62a4122a500650fef99eed0a918
Chris S <mrshrinkray@gmail.com>
2015-02-06 10:29:12 +0000

    Fixed solution paths and missing files

---
+++ src/NeuralNetwork.Tests/MCPNeuronTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NeuralNetwork.Tests
+{
+	public class MCPNeuronTests
+	{
+		[Test]
+		public void getoutput_should_return_0_when_inhibitory_value_is_greater_than_1()
+		{
+			// Arrange
+			int expectedOutput = 0;
+			MCPNeuron neuron = new MCPNeuron();
+
+			neuron.ExcitatoryInputs.Add(1);
+			neuron.ExcitatoryInputs.Add(2);
+			neuron.InhibitoryInputs.Add(1);
+
+			// Act
+			int actualOutput = neuron.GetOutput();
+
+			// Assert
+			Assert.That(expectedOutput, Is.EqualTo(actualOutput));
+		}
+
+		[Test]
+		public void getoutput_should_return_1_when_no_inhibitory_value()
+		{
+			// Arrange
+			int expectedOutput = 1;
+			MCPNeuron neuron = new MCPNeuron();
+
+			neuron.ExcitatoryInputs.Add(1);
+			neuron.ExcitatoryInputs.Add(2);
+
+			// Act
+			int actualOutput = neuron.GetOutput();
+
+			// Assert
+			Assert.That(expectedOutput, Is.EqualTo(actualOutput));
+		}
+
+		[Test]
+		public void getoutput_should_return_0_when_theshold_value_is_not_reached()
+		{
+			// Arrange
+			int expectedOutput = 0;
+			MCPNeuron neuron = new MCPNeuron();
+
+			neuron.Threshold = 4;
+			neuron.ExcitatoryInputs.Add(1);
+			neuron.ExcitatoryInputs.Add(2);
+
+			// Act
+			int actualOutput = neuron.GetOutput();
+
+			// Assert
+			Assert.That(expectedOutput, Is.EqualTo(actualOutput));
+		}
+
+		// Excitatory values can also be used for truth tables
+	}
+}
